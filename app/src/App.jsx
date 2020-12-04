f0e3c134cf6d18d9b3e4b8470b8867e71a032ba6
S Larsen <sverre.christofferfotball@hotmail.com>
2020-12-04 14:32:40 +0100

    new file:   app/package-lock.json 	modified:   app/src/App.jsx 	modified:   app/src/index.html 	modified:   app/src/index.js

---
+++ app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Theme from './Components/Styles/Theme';
+import Routes from './Components/Routes/Routes';
+
+const App = () => (
+  <Routes/>
+);
+
+export default App;
