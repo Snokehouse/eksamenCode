e4c6ff4fb0d9c6590889ae311679a49c2838cdf4
S Larsen <sverre.christofferfotball@hotmail.com>
2020-12-04 14:22:02 +0100

    new file:   app/.env 	new file:   app/.eslintignore 	new file:   app/.eslintrc.json 	new file:   app/.gitignore 	new file:   app/.vscode/extensions.json 	new file:   app/.vscode/settings.json 	new file:   app/babel.config.json 	new file:   app/jest.config.js 	new file:   app/package copy.json 	new file:   app/package.json 	new file:   app/webpack.common.config.js 	new file:   app/webpack.development.config.js 	new file:   app/webpack.production.config.js

---
+++ app/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    collectiveCoverageFrom: [
+        'src/**/*.js?(x)'
+    ],
+
+    setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
+
+    testEnvironment: "jsdom",
+
+    testMatch: [
+        "<rootDir>/test/**/?(*.)+(spec|test).[tj]s?(x)"
+    ],
+
+}
