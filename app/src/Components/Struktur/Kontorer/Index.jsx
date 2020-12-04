26bbeb5646065fbd2941c119ad09363bca9c9515
S Larsen <sverre.christofferfotball@hotmail.com>
2020-12-04 19:31:10 +0100

    modified:   app/src/Components/Struktur/Kontorer/Index.jsx 	modified:   app/src/Components/Struktur/Kontorer/Style.jsx

--- app/src/Components/Struktur/Kontorer/Index.jsx
+++ app/src/Components/Struktur/Kontorer/Index.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import { Container, Tittel, Filter, KnappListe, KnappCard } from './Style';
 
-const Kontorer = () => <h2>404 Error not found.</h2>;
-
+const Kontorer = () => (
+  // hmm
+  <>
+    <Tittel>Våre Kontorer</Tittel>
+    <Container>
+      <Filter>Filter</Filter>
+      <KnappListe>List</KnappListe>
+      <KnappCard>Card</KnappCard>
+    </Container>
+    <Container>
+      {
+        // sjekke hvilken liste som skal genereres (List eller Card)
+        // så sjekke om det er noe data, vis ikke generere statisk tekst
+        //
+        //
+        /* pollData.length < 1 ? (
+        <p>Ingen polls idag :(</p>
+      ) : (
+        pollData.map((
+          poll // sette id til poll setId(data[0]._id);
+        ) => (
+          <PollForm
+            key={poll.sporsmal}
+            submitHandle={submitHandle}
+            svarData={svarData}
+            updateValue={updateValue}
+            poll={poll}
+          />
+        ))
+        ) */}
+    </Container>
+  </>
+);
 export default Kontorer;
--- app/src/Components/Struktur/Kontorer/Style.jsx
+++ app/src/Components/Struktur/Kontorer/Style.jsx
@@ -3,12 +3,29 @@ import styled from 'styled-components';
 export const Container = styled.section`
   margin: 0 auto;
   width: 100%;
-  background-color: grey;
 `;
-export const HomeArticle = styled.article`
+export const Tittel = styled.h1`
+  margin: 0;
+`;
+
+export const UnderTittel = styled.h2`
+  margin: 0;
+`;
+
+export const KontorArticle = styled.article`
   margin: 0 auto;
   background-color: grey;
 `;
-export const Paragraph = styled.p`
+export const Paragraf = styled.p`
   margin: 0 auto;
+`;
+
+export const Filter = styled.button`
+  margin: 0;
+`;
+export const KnappListe = styled.button`
+  margin: 0;
+`;
+export const KnappCard = styled.button`
+  margin: 0;
 `;
