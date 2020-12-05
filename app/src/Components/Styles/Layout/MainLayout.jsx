09afd091363ddd37cc88a6f13dd34898cd435c88
S Larsen <sverre.christofferfotball@hotmail.com>
2020-12-05 14:44:29 +0100

    modified:   app/src/Components/Styles/Layout/MainLayout.jsx 	deleted:    app/src/Components/Styles/Styled.jsx

--- app/src/Components/Styles/Layout/MainLayout.jsx
+++ app/src/Components/Styles/Layout/MainLayout.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
-import { Container } from '../Styled';
 
 import Nav from './Nav/Index';
 import Footer from './Footer/Index';
 
+const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+`;
 const MainLayoutContainer = styled.div`
   padding-bottom: 20px;
   min-height: 100%;
   background-color: #fff;
 `;
-
 const StyledHeader = styled.header`
   background: #fff;
   box-shadow: 1px 1px 2px #f5f5f5;
