00f6b133887bf1d272465711a0f248fe8ff44a72
S Larsen <sverre.christofferfotball@hotmail.com>
2020-12-05 22:47:49 +0100

    modified:   app/src/Components/Struktur/Fagartikler/ArtikkelOversikt/Index.jsx 	new file:   app/src/Components/Struktur/Fagartikler/NyArtikkel/Style.jsx 	modified:   app/src/Components/Struktur/Fagartikler/NyArtikkel/index.jsx 	modified:   server/Route/Kontorer.js 	modified:   server/server.js

--- app/src/Components/Struktur/Fagartikler/NyArtikkel/index.jsx
+++ app/src/Components/Struktur/Fagartikler/NyArtikkel/index.jsx
@@ -1,5 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NyArtikkel = () => <h2>404 ny artikkel not found.</h2>;
+import { Container, Tittel } from './Style';
+
+const NyArtikkel = () => {
+  const [formdata, setFormdata] = useState([
+    {
+      id: 0,
+      tittel: '',
+      dato: '',
+      beskrivelse: '',
+      kategori: '',
+      forfatter: '',
+    },
+  ]);
+
+  const updateValue = (event) => {
+    const inputValue = { [event.target.name]: event.target.value };
+    setFormdata((prev) => ({
+      ...prev,
+      ...inputValue,
+    }));
+  };
+  const submitHandle = (event) => {
+    console.log('Du prøvde å sende inn ny artikkel!');
+    /* event.preventDefault();
+    pollData.author = inloggetData.id;
+    const createData = async () => {
+      const { data, error } = await create(pollData);
+      if (error) {
+        console.log(error);
+        setError(error);
+      } else {
+        setPollData({ sporsmal: '' });
+      }
+    };
+    createData(); */
+  };
+
+  return (
+    <>
+      <Tittel>Ny Artikkel</Tittel>
+      <Container>
+        <form onSubmit={submitHandle}>
+          <label htmlFor="txtTittel">Tittel: </label>
+          <input
+            id="txtTittel"
+            name="tittel"
+            type="text"
+            value={formdata.tittel}
+            onChange={updateValue}
+          />
+          <label htmlFor="txtDato">Dato: </label>
+          <input
+            id="txtDato"
+            name="dato"
+            type="date"
+            value={formdata.dato}
+            onChange={updateValue}
+          />
+          <label htmlFor="txtBeskrivelse">Beskrivelse: </label>
+          <input
+            id="txtBeskrivelse"
+            name="beskrivelse"
+            type="text"
+            value={formdata.beskrivelse}
+            onChange={updateValue}
+          />
+
+          <label htmlFor="txtForfatter">Forfatter: </label>
+          <input
+            id="txtForfatter"
+            name="forfatter"
+            type="text"
+            value={formdata.forfatter}
+            onChange={updateValue}
+          />
+        </form>
+      </Container>
+    </>
+  );
+};
 
 export default NyArtikkel;
--- server/Route/Kontorer.js
+++ server/Route/Kontorer.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { writeJson } from '../Controller/Kontorer.js';
-import router from './admin.js';
 
 const router = express.Router();
 
