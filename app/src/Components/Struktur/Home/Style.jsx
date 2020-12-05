3ea0227ca80b87f84e2e4d79ad7b1d9d99518cea
onsyveri <ole.syverinsen@googlemail.com>
2020-12-05 12:41:31 +0100

    modified:   app/src/Components/Struktur/Home/Style.jsx

--- app/src/Components/Struktur/Home/Style.jsx
+++ app/src/Components/Struktur/Home/Style.jsx
@@ -4,13 +4,37 @@ export const Tittel = styled.h1`
   margin: 0;
 `;
 export const Container = styled.section`
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+  grid-template-rows: auto;
+  grid-template-areas: 
+
+    "main . sidebar sidebar"
+    "footer footer footer footer";
+
+  
   margin: 0 auto;
   width: 100%;
-  background-color: grey;
+  background-color: pink;
 `;
 export const HomeArticle = styled.article`
   margin: 0 auto;
   background-color: grey;
+
+  &:first-child {
+    grid-area: main;
+    color: green;
+  }
+
+  &:nth-child(2) {
+      grid-area: sidebar;
+      color:yellow;
+  }
+
+  &:nth-child(3) {
+      grid-area: footer;
+  }
+
 `;
 export const Paragraph = styled.p`
   margin: 0 auto;
