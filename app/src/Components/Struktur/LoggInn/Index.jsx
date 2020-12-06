1f24af99832419ef5137d512180c2a19c6311228
S Larsen <sverre.christofferfotball@hotmail.com>
2020-12-06 12:55:54 +0100

    modified:   app/src/Components/Struktur/LoggInn/Index.jsx

--- app/src/Components/Struktur/LoggInn/Index.jsx
+++ app/src/Components/Struktur/LoggInn/Index.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import { Container, Paragraph, Felter } from './Style.jsx';
 
 const LoggInn = () => (
-
-<>
-
+  <>
     <Container>
-        <Felter>
-            <Paragraph>Yoyo this is ya boy checking inn</Paragraph>
-        </Felter>
+      <Felter>
+        <Paragraph>Yoyo this is ya boy checking inn</Paragraph>
+      </Felter>
     </Container>
-
-</>
-
+  </>
 );
 
 export default LoggInn;
