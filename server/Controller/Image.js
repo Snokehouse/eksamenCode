573b9413b076d945446631d3ce1a42da1b25884c
S Larsen <sverre.christofferfotball@hotmail.com>
2020-12-07 18:26:54 +0100

    modified:   server/Controller/Image.js 	modified:   server/Modell/Image.js     modified:   server/Route/Image.js 	modified:   server/Service/Image.js 	modified:   server/server.js

--- server/Controller/Image.js
+++ server/Controller/Image.js
@@ -4,25 +4,25 @@ import catchAsyncErrors from '../middleware/catchAsync.js';
 import ErrorHandler from '../utils/errorHandler.js';
 
 export const create = catchAsyncErrors(async (req, res, next) => {
-    if (!req.file) {
-        return next(new ErrorHandler('Last opp en bildefil', 400));
-    }
-    const image = await imageService.uploadImage(req.file);
-    res.status(201).json({
-        success: true,
-        data: image,
-    });
+  if (!req.file) {
+    return next(new ErrorHandler('Last opp en bildefil', 400));
+  }
+  const image = await imageService.uploadImage(req.file);
+  res.status(201).json({
+    success: true,
+    data: image,
+  });
 });
 
 export const get = catchAsyncErrors(async (req, res, next) => {
-    const image = await imageService.getImageById(req.params.id);
-    if(!image) {
-        return next(new ErrorHandler('Fant ikke bildefil', 404));
-    }
+  const image = await imageService.getImageById(req.params.id);
+  if (!image) {
+    return next(new ErrorHandler('Fant ikke bildefil', 404));
+  }
 
-    res.set({
-        'Content-Type': image.file_mimetype,
-    });
+  res.set({
+    'Content-Type': image.file_mimetype,
+  });
 
-    res.sendFile(path.join(_dirname, '..', image.file_path));
+  res.sendFile(path.join(_dirname, '..', image.file_path));
 });
--- server/server.js
+++ server/server.js
@@ -6,62 +6,65 @@ import { PORT } from './Constants/index.js';
 import 'dotenv/config.js';
 import errorMiddleware from './middleware/errors.js';
 
-
-import connectDatabase from './Config/db.js'
+import connectDatabase from './Config/db.js';
 import admin from './Route/Admin.js';
 import kontorer from './Route/Kontorer.js';
 import artikkel from './Route/Artikkel.js';
 import kategori from './Route/Kategori.js';
 import forfatter from './Route/Forfatter.js';
-//import image from './Route/Image.js';
+// import image from './Route/Image.js';
 
-//lager "app" med express slik at vi får satt opp server
+// lager "app" med express slik at vi får satt opp server
 const app = express();
-//initialiserer at app skal bruke json
+// initialiserer at app skal bruke json
 app.use(express.json());
 
-//morgan for bedre feilmeldinger i development mode
+// morgan for bedre feilmeldinger i development mode
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-//bruker cors til å tilate trafikk fra client?
-app.use(cors({
-  origin: 'http://localhost:3000',
-  allowedHeaders: ['Content-Type']
-}))
-
-
-//url som blir brukt localhost:5000/api/v1/admin
-app.use(`${process.env.BASEURL}/admin`, admin);
-//url som blir brukt localhost:5000/api/v1/kontorer
-app.use(`${process.env.BASEURL}/kontorer`, kontorer);
-
-//url som blir brukt localhost:5000/api/v1/kontorer
-app.use(`${process.env.BASEURL}/forfatter`, forfatter);
-
-//url som blir brukt localhost:5000/api/v1/artikkel
-app.use(`${process.env.BASEURL}/artikkel`, artikkel);
-
-//url som blir brukt localhost:5000/api/v1/kategori
-app.use(`${process.env.BASEURL}/kategori`, kategori);
-
-//global error håndtering
-app.use(errorMiddleware);
-
-//koble til databasen
-connectDatabase();
-
-//se hvilken url det er til server
-console.log(`URL: http://localhost:5000${process.env.BASEURL}/`)
-
-//server for apiet
-const server = app.listen(
-  PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+// bruker cors til å tilate trafikk fra client?
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type'],
+  })
 );
 
-//for å håndtere uhåndterte errors, stopper serveren
+// url som blir brukt localhost:5000/api/v1/admin
+app.use(`${process.env.BASEURL}/admin`, admin);
+
+// url som blir brukt localhost:5000/api/v1/kontorer
+app.use(`${process.env.BASEURL}/kontorer`, kontorer);
+
+// url som blir brukt localhost:5000/api/v1/kontorer
+app.use(`${process.env.BASEURL}/forfatter`, forfatter);
+
+// url som blir brukt localhost:5000/api/v1/artikkel
+app.use(`${process.env.BASEURL}/artikkel`, artikkel);
+
+// url som blir brukt localhost:5000/api/v1/kategori
+app.use(`${process.env.BASEURL}/kategori`, kategori);
+
+// global error håndtering
+app.use(errorMiddleware);
+
+// koble til databasen
+connectDatabase();
+
+// se hvilken url det er til server
+console.log(`URL: http://localhost:5000${process.env.BASEURL}/`);
+
+// server for apiet
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+
+// for å håndtere uhåndterte errors, stopper serveren
 process.on('unhandledRejection', (err) => {
   console.log(`Error: ${err.message}`);
   console.log('Shutting down server due to Unhandled Promise Rejection');
