e03c049a6f998fbcb97ddc51d94cbc708b6a0d30
onsyveri <ole.syverinsen@googlemail.com>
2020-12-10 11:42:20 +0100

    modified:   app/src/Components/Styles/Layout/Nav/Index.jsx

--- app/src/Components/Styles/Layout/Nav/Index.jsx
+++ app/src/Components/Styles/Layout/Nav/Index.jsx
@@ -88,7 +88,7 @@ const Nav = () => {
     await logout();
     setUser(null);
   };
-  console.log(NavMenu.getComputedStyle().getPropertyValue('display'));
+  //console.log(NavMenu.getComputedStyle().getPropertyValue('display'));
 
   return (
     <>
